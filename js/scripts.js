17395fa9c51c59b59feb604637b2081b2047d1d0
Rich Wells <rich@yoomee.com>
2017-03-22 23:41:07 +0000

    Update the layout and about page

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     //	Scroll to top link
     $.scrollUp();
     $('#scrollUp').html("<i class='fa fa-angle-up'></i>");
-    
+
 
     // initialize Isotope after all images have loaded
     var $container = $('#isotope').imagesLoaded( function() {
@@ -17,6 +17,18 @@ $(document).ready(function() {
         layoutMode: 'masonry'
       });
     });
+
+
+    // spinny thing
+      var $cog = $('.freelance-cog-img'),
+      $body = $(document.body),
+      bodyHeight = $body.height();
+
+      $(window).scroll(function () {
+        $cog.css({
+            'transform': 'rotate(' + ($body.scrollTop() / bodyHeight * 560) + 'deg)'
+        });
+      });
 
 
 
