1c200c48f73e78cffd583bfc026fa22b24c27626
Rich Wells <iamrichwells@gmail.com>
2017-09-19 19:55:43 +0100

    update changemakers js

--- timetochange/scripts/changemakers.js
+++ timetochange/scripts/changemakers.js
@@ -3,19 +3,28 @@ $(document).ready(function(){
 			$(".filter-strip").toggleClass("active");
 	 });
 
+	 // initialize Isotope after all images have loaded
+	        var $container = $('#content.cm-wrapper').imagesLoaded( function() {
+	          $container.isotope({
+	            // options
+	            itemSelector: '.cm-box',
+	            layoutMode: 'masonry'
+	          });
+	        });
 
-			 // wait for twitter embeds to load and then refresh masonry (needs refining)
-			       twttr.events.bind(
-			         'rendered',
-			         function (event) {
-			           var $container = $('#content.cm-wrapper').imagesLoaded( function() {
-			             $container.isotope({
-			               // options
-			               itemSelector: '.cm-box',
-			               layoutMode: 'masonry'
-			             });
-			           });
-			         }
-			       );
+
+	 // wait for twitter embeds to load and then refresh masonry
+	       twttr.events.bind(
+	         'rendered',
+	         function (event) {
+	           var $container = $('#content.cm-wrapper').imagesLoaded( function() {
+	             $container.isotope({
+	               // options
+	               itemSelector: '.cm-box',
+	               layoutMode: 'masonry'
+	             });
+	           });
+	         }
+	       );
 
 });
